284fb509132df7fd94c7b516ba0389d2e5c20ce2
koike.engr <hkz2.engineering.19346@gmail.com>
2021-01-22 09:53:14 +0900

    fix: そもそもdbディレクトリがconfig配下にあるのがおかしかった

--- app/db/migrate/20210121230438_create_scflds.rb
+++
@@ -1,8 +0,0 @@
-class CreateScflds < ActiveRecord::Migration[5.2]
-  def change
-    create_table :scflds do |t|
-
-      t.timestamps
-    end
-  end
-end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_21_010042) do
+
+  create_table "vtubers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.float "subscribers"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "superchats"
+  end
 
 end
